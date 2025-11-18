b19ed33ed15afa45a553569496ffb476e6d7f0ad
Anton Wretenberg <Anton.Wretenberg@outlook.com>
2025-11-18 13:42:39 +0100

    Created the first test BugDoesNotHash

--- js/practice/practice_write_run_tests/tests/password.test.js
+++ js/practice/practice_write_run_tests/tests/password.test.js
@@ -1,8 +1,8 @@
-
+import { describe, it, expect} from 'vitest'
 // Select one of the Password versions to test
 
-import { Password } from '../src/BugDoesNotHash'
-// import { Password } from '../src/BugDoesNotTrim'
+// import { Password } from '../src/BugDoesNotHash'
+//import { Password } from '../src/BugDoesNotTrim'
 // import { Password } from '../src/BugisPasswordAlwaysSame'
 // import { Password } from '../src/BugMissingNumberCheck'
 // import { Password } from '../src/BugMissingPasswordCheck'
@@ -11,15 +11,18 @@ import { Password } from '../src/BugDoesNotHash'
 // import { Password } from '../src/BugVeryShort'
 // import { Password } from '../src/BugWrongHashingAlgorithm'
 // import { Password } from '../src/BugWrongMessage'
-// import { Password } from '../src/Correct'
+import { Password } from '../src/Correct'
 
 describe('Password class, test suite', () => {
     //put constants here to increase readability
-    const emptyPassword = '';
+    const myPassword = 'mysecurepassword123'
+    const password = new Password(myPassword)
 
-    test('replace this test with one of your own and add more', () => {
-        expect(true).toBe(true);
-    });
+    it('Check if the password is hashed', () => {
+        const hashedPassword = password.getPasswordHash()
+
+        expect(hashedPassword).not.toBe(myPassword)
+    })
 
     //Add your tests here
 });
