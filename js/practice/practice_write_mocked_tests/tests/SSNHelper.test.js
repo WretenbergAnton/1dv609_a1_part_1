77b4e65cb28460b84df0c13a1c2cffa65526883e
Anton Wretenberg <Anton.Wretenberg@outlook.com>
2025-11-20 14:38:41 +0100

    Created the test for the length should be exactly 11

--- js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
+++ js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
@@ -1,55 +1,58 @@
-//import { SSNHelper } from '../src/correct/SSNHelper';
-//import { SSNHelper } from '../src/bugs/BuggySSNHelperAllowDayUpTo30'; 
+import { SSNHelper } from '../src/correct/SSNHelper';
+//import { SSNHelper } from '../src/bugs/BuggySSNHelperAllowDayUpTo30';
 //import { SSNHelper } from '../src/bugs/BuggySSNHelperAllowMonth0';
-import { SSNHelper } from '../src/bugs/BuggySSNHelperIncorrectFormat';
+//import { SSNHelper } from '../src/bugs/BuggySSNHelperIncorrectFormat';
+//import { SSNHelper } from "../src/bugs/BuggySSNHelperMessyLuhn";
+//import { SSNHelper } from '../src/bugs/BuggySSNHelperWrongLength';
 
+describe("SSNHelpe Tests", () => {
+  const helper = new SSNHelper()
 
+  const correctFormat = "123456-1234";
+  const wrongFormat = "123456-1234132";
+  const invalidSSNs = ["1234567890", "123456-123", "123456-12345", ""]
 
+  test("SSNHelper Allow Day From 1-31 Days", () => {
+    for (let days = 1; days <= 31; days++) {
+      const isValid = helper.isValidDay(days)
+      expect(isValid).toBeTruthy()
+    }
+  })
 
-describe('SSNHelpe Tests', () => {
+  test("SSNHelper Do Not Allow Month To Be 0", () => {
+    const isNotValid = helper.isValidMonth(0)
+    expect(isNotValid).toBeFalsy()
+  })
 
-    const helper = new SSNHelper()
+  test("SSNHelper Allow Month Between 1-12", () => {
+    for (let months = 1; months <= 12; months++) {
+      const isValid = helper.isValidMonth(months)
+      expect(isValid).toBeTruthy()
+    }
+  })
 
-    const correctFormat = '123456-1234'
-    const invalidSSNs = [
-        '1234567890',
-        '123456-123',
-        '123456-12345',
-        ''
-    ]
+  test("The Format Of An SSN Should Be 123456-1234", () => {
+    const isValid = helper.isCorrectFormat(correctFormat)
+    expect(isValid).toBeTruthy()
+  })
 
-    test('SSNHelper Allow Day From 1-31 Days', () => {
-        for(let days = 1; days <= 31; days++) {
-            const isValid = helper.isValidDay(days)
-            expect(isValid).toBeTruthy()
-        }
+  test("The Format Of An SSN Should Allways Be 123456-1234 If Not Throw Error", () => {
+    invalidSSNs.forEach((ssn) => {
+      const isValid = helper.isCorrectFormat(ssn)
+      expect(isValid).toBeFalsy()
     })
+  })
 
-    test('SSNHelper Do Not Allow Month To Be 0', () => {
-        const isNotValid = helper.isValidMonth(0)
-        expect(isNotValid).toBeFalsy()
-    })
+  test('The Length Of A Security Number Should Be Exactly 11', () => {
+    const isValid = helper.isCorrectLength(correctFormat)
 
-    test('SSNHelper Allow Month Between 1-12', () => {
-        for(let months = 1; months <= 12; months++) {
-            const isValid = helper.isValidMonth(months)
-            expect(isValid).toBeTruthy()
-        }
-    })
+    expect(isValid).toBeTruthy()
+  })
 
-    test('The Format Of An SSN Should Be 123456-1234', () => {
-        const isValid = helper.isCorrectFormat(correctFormat)
-        expect(isValid).toBeTruthy()
-    })
+    test('The Length Of A Security Number Should NOT Be More Then 11', () => {
+    const isValid = helper.isCorrectLength(wrongFormat)
 
-    test('The Format Of An SSN Should Allways Be 123456-1234 If Not Throw Error', () => {
-        invalidSSNs.forEach(ssn => {
-            const isValid = helper.isCorrectFormat(ssn)
-            expect(isValid).toBeFalsy()
-        })
-    })
-
-
-
-    //Add your tests here
+    expect(isValid).toBeFalsy()
+  })
+  //Add your tests here
 });
