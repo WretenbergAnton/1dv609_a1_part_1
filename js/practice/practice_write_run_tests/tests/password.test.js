4b879d9d3565d438ad9ddd0f9f84be7aba11c435
Anton Wretenberg <Anton.Wretenberg@outlook.com>
2025-11-20 10:33:40 +0100

    Created a test for BugNeverContainsNumbers

--- js/practice/practice_write_run_tests/tests/password.test.js
+++ js/practice/practice_write_run_tests/tests/password.test.js
@@ -1,6 +1,6 @@
 // Select one of the Password versions to test
 
-// import { Password } from '../src/BugDoesNotHash'
+//import { Password } from '../src/BugDoesNotHash'
 //import { Password } from '../src/BugDoesNotTrim'
 //import { Password } from '../src/BugisPasswordAlwaysSame'
 //import { Password } from '../src/BugMissingNumberCheck'
@@ -9,11 +9,11 @@
 //import { Password } from '../src/BugToShortPassword'
 //import { Password } from '../src/BugVeryShort'
 //import { Password } from '../src/BugWrongHashingAlgorithm'
- import { Password } from '../src/BugWrongMessage'
-//import { Password } from '../src/Correct'
+//import { Password } from '../src/BugWrongMessage'
+import { Password } from '../src/Correct'
 
 describe('Password class, test suite', () => {
-    //put constants here to increase readability
+     //put constants here to increase readability
     const pw1 = 'mysecurepassword123'
     const password1 = new Password(pw1)
 
@@ -37,6 +37,10 @@ describe('Password class, test suite', () => {
         expect(() => new Password('passwordwithnonumbers')).toThrow('No number found')        
     })
 
+    test('Check if password is using num but still not pass', () => {
+        expect(() => new Password('validpassword123')).not.toThrow()
+    })
+
     test('Check if a password needs to be at least 12 long', () => {
         expect(() => new Password('shortpw8')).toThrow("Too short password")
     })
@@ -57,5 +61,4 @@ describe('Password class, test suite', () => {
     test('Check if the Error message is "Too short password"', () => {
         expect(() => new Password('asklfjd')).toThrow('Too short password')
     })
-    //Add your tests here
 });
