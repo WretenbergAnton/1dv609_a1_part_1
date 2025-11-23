3421dcde52a91ee00d57e9fe1694e63d1c4847d2
Anton Wretenberg <Anton.Wretenberg@outlook.com>
2025-11-23 14:40:47 +0100

    Changed the test names for SSNHelper

--- js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
+++ js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
@@ -5,60 +5,59 @@ import { SSNHelper } from '../src/correct/SSNHelper';
 //import { SSNHelper } from "../src/bugs/BuggySSNHelperMessyLuhn";
 //import { SSNHelper } from '../src/bugs/BuggySSNHelperWrongLength';
 
-describe("SSNHelpe Tests", () => {
-  const helper = new SSNHelper()
+import { SSNHelper } from '../src/correct/SSNHelper';
+
+describe("SSNHelper Tests", () => {
+  const helper = new SSNHelper();
 
   const correctFormat = "890201-3286";
   const wrongFormat = "123456-1234132";
-  const invalidSSNs = ["1234567890", "123456-123", "123456-12345", ""]
+  const invalidSSNs = ["1234567890", "123456-123", "123456-12345", ""];
 
-  test("SSNHelper allow day from 1-31 days", () => {
+  test("should allow days from 1 to 31", () => {
     for (let days = 1; days <= 31; days++) {
-      const isValid = helper.isValidDay(days)
-      expect(isValid).toBeTruthy()
+      const isValid = helper.isValidDay(days);
+      expect(isValid).toBeTruthy();
     }
-  })
+  });
 
-  test("SSNHelper do not allow month to be 0", () => {
-    const isNotValid = helper.isValidMonth(0)
-    expect(isNotValid).toBeFalsy()
-  })
+  test("should not allow month to be 0", () => {
+    const isNotValid = helper.isValidMonth(0);
+    expect(isNotValid).toBeFalsy();
+  });
 
-  test("SSNHelper allow month between 1-12", () => {
+  test("should allow months between 1 and 12", () => {
     for (let months = 1; months <= 12; months++) {
-      const isValid = helper.isValidMonth(months)
-      expect(isValid).toBeTruthy()
+      const isValid = helper.isValidMonth(months);
+      expect(isValid).toBeTruthy();
     }
-  })
+  });
 
-  test("The format of an SSN should be 123456-1234", () => {
-    const isValid = helper.isCorrectFormat(correctFormat)
-    expect(isValid).toBeTruthy()
-  })
+  test("should accept SSN format 123456-1234", () => {
+    const isValid = helper.isCorrectFormat(correctFormat);
+    expect(isValid).toBeTruthy();
+  });
 
-  test("The format of an SSN should allways be 123456-1234 if not throw error", () => {
+  test("should reject incorrect SSN formats", () => {
     invalidSSNs.forEach((ssn) => {
-      const isValid = helper.isCorrectFormat(ssn)
-      expect(isValid).toBeFalsy()
-    })
-  })
+      const isValid = helper.isCorrectFormat(ssn);
+      expect(isValid).toBeFalsy();
+    });
+  });
 
-  test('The length of a security number should be exactly 11', () => {
-    const isValid = helper.isCorrectLength(correctFormat)
+  test("should accept length of exactly 11 characters", () => {
+    const isValid = helper.isCorrectLength(correctFormat);
+    expect(isValid).toBeTruthy();
+  });
 
-    expect(isValid).toBeTruthy()
-  })
+  test("should reject SSNs longer than 11 characters", () => {
+    const isValid = helper.isCorrectLength(wrongFormat);
+    expect(isValid).toBeFalsy();
+  });
 
-    test('The length of A security number should not be more then 11', () => {
-    const isValid = helper.isCorrectLength(wrongFormat)
+  test("should validate a correct SSN using the Luhn algorithm", () => {
+    const result = helper.luhnisCorrect(correctFormat);
+    expect(result).toBeTruthy();
+  });
 
-    expect(isValid).toBeFalsy()
-  })
-
-  test('Test if a security number is valid using luhn algorithm', () => {
-    const result = helper.luhnisCorrect(correctFormat)
-
-    expect(result).toBeTruthy()
-  })
-  //Add your tests here
 });
