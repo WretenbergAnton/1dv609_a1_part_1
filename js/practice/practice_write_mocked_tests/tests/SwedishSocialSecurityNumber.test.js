44204c8cbcba48adb05595142e833b2a65eb22ee
Anton Wretenberg <Anton.Wretenberg@outlook.com>
2025-11-23 14:32:34 +0100

    Created the table for the first part also fixed some issues with the first test on password

--- js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
+++ js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
@@ -1,14 +1,18 @@
 import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber'; 
+//import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoLenCheck'; 
+//import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoTrim'; 
+//import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberNoLuhn'; 
+//import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberWrongYear'; 
 
 
 
 //NOTE THESE TESTS SHOULD NOT BE DEPENDENT ON SSNHelper BUT USE MOCKING
 describe('SwedishSocialSecurityNumber Tests', () => {
-    //put constants here to increase readability
 
-    test('replace this test with one of your own', () => {
-        expect(true).toBe(true);
-    });
+    test('dfa', () => {
+        expect(true).toBe(true)
+    })
+
 
     //Add your tests here
 });
--- js/practice/practice_write_run_tests/tests/password.test.js
+++ js/practice/practice_write_run_tests/tests/password.test.js
@@ -1,69 +1,72 @@
 // Select one of the Password versions to test
 
+//import { Password } from '../src/Correct'
+
 //import { Password } from '../src/BugDoesNotHash'
 //import { Password } from '../src/BugDoesNotTrim'
 //import { Password } from '../src/BugisPasswordAlwaysSame'
 //import { Password } from '../src/BugMissingNumberCheck'
 //import { Password } from '../src/BugMissingPasswordCheck'
-//import { Password } from '../src/BugNeverContainsNumbers'
+import { Password } from '../src/BugNeverContainsNumbers'
 //import { Password } from '../src/BugToShortPassword'
 //import { Password } from '../src/BugVeryShort'
 //import { Password } from '../src/BugWrongHashingAlgorithm'
 //import { Password } from '../src/BugWrongMessage'
-import { Password } from '../src/Correct'
 
 describe('Password class, test suite', () => {
-     //put constants here to increase readability
+    // Constants for readability
     const pw1 = 'mysecurepassword123'
     const password1 = new Password(pw1)
 
-    test('Check if the password is hashed', () => {
+    test('should hash the password', () => {
         const hashedPassword = password1.getPasswordHash()
-
         expect(hashedPassword).not.toBe(pw1)
     })
 
-    test('check if the trim works', () => {
+    test('should trim whitespace from password', () => {
         const pwWithSpaces = new Password(' mysecurepassword123 ')
         expect(pwWithSpaces.isPasswordSame(password1)).toBe(true)
     })
 
-    test('Check if the password is allways the same', () => {
-        const newPassword = new Password('wrongpassword123')
-        expect(newPassword.isPasswordSame(password1)).toBe(false)
+    test('should return false when comparing different passwords', () => {
+        const secondPassword = new Password('secondpw12345')
+
+        const arePasswordsTheSame = password1.isPasswordSame(secondPassword)
+
+        expect(arePasswordsTheSame).toBeFalsy()
     })
 
-    test('Check if number is required in a password', () => {
+    test('should throw error if password has no numbers', () => {
         expect(() => new Password('passwordwithnonumbers')).toThrow('No number found')        
     })
 
-    test('Check if password is using num but still not pass', () => {
+    test('should not throw error for valid password', () => {
         expect(() => new Password('validpassword123')).not.toThrow()
     })
 
-    test('Check if a password needs to be at least 12 long', () => {
-        expect(() => new Password('shortpw8')).toThrow("Too short password")
+    test('should require password to be at least 12 characters long', () => {
+        expect(() => new Password('shortpw8')).toThrow('Too short password')
     })
 
-    test('Check if password is below 11 charachters', () => {
+    test('should reject password with 11 characters', () => {
         expect(() => new Password('password123')).toThrow('Too short password')
     })
 
-    test('Check if password is below 6 charachters', () => {
+    test('should reject password with 4 characters', () => {
         expect(() => new Password('pw12')).toThrow('Too short password')
     })
 
-    test('different passwords must generate different hashes', () => {
-        const password2 = new Password('anotherpassword123')
-
-        expect(password1.getPasswordHash()).not.toBe(password2.getPasswordHash())
+    test('should generate different hashes for different passwords', () => {
+        const pw1 = new Password('firstpw123456')
+        const pw2 = new Password('secondpw12345')
+        expect(pw1.getPasswordHash()).not.toBe(pw2.getPasswordHash())
     })
 
-    test('Check if the Error message is "Too short password"', () => {
-        expect(() => new Password('asklfjd')).toThrow('Too short password')
+    test('should throw correct error message for short password', () => {
+        expect(() => new Password('short1')).toThrow('Too short password')
     })
 
-    test('Check if using wrong password will return an Error message', () => {
+    test('should throw "Invalid argument" when comparing with non-Password object', () => {
         expect(() => password1.isPasswordSame('wrongPw')).toThrow('Invalid argument')
     })
 });
