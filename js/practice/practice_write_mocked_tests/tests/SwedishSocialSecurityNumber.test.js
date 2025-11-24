4e9daa85d96bd1fc819a2e613ab467136e20d8ae
Anton Wretenberg <Anton.Wretenberg@outlook.com>
2025-11-24 14:27:21 +0100

    Started on the test using mocks

--- js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
+++ js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
@@ -1,18 +1,42 @@
-import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber'; 
-//import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoLenCheck'; 
-//import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoTrim'; 
-//import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberNoLuhn'; 
-//import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberWrongYear'; 
+import { expect, jest } from '@jest/globals'
+
+
+//import { SwedishSocialSecurityNumber } from "../src/correct/SwedishSocialSecurityNumber"
+//import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoLenCheck'
+import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoTrim'
+//import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberNoLuhn'
+//import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberWrongYear'
+
+describe("SwedishSocialSecurityNumber Tests", () => {
+
+  const correctFormat = "890201-3286"
+  const correctFormatWithSpaces = " 890201-3286"
+  const ssnWithInvalidLuhn = "890201-3285"
+
+  let mockHelper
+  
+  beforeEach(() => {
+
+   mockHelper = {
+    isCorrectLength: jest.fn(),
+    isCorrectFormat: jest.fn(),
+    isValidMonth: jest.fn(),
+    isValidDay: jest.fn(),
+    luhnisCorrect: jest.fn(),
+  }
+
+})
+
+  test('should throw if length is incorrect', () => {
+    mockHelper.isCorrectLength.mockReturnValue(false)
+
+    mockHelper.isCorrectFormat.mockReturnValue(true)
+
+    expect(() => {
+      new SwedishSocialSecurityNumber(correctFormat, mockHelper)
+    }).toThrow("To short, must be 11 characters")
+  })
 
 
 
-//NOTE THESE TESTS SHOULD NOT BE DEPENDENT ON SSNHelper BUT USE MOCKING
-describe('SwedishSocialSecurityNumber Tests', () => {
-
-    test('dfa', () => {
-        expect(true).toBe(true)
-    })
-
-
-    //Add your tests here
-});
+})
