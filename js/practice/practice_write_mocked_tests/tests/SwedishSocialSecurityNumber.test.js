76b4c1488d3e858d8533ef99f316e33d6a11cf79
Anton Wretenberg <Anton.Wretenberg@outlook.com>
2025-11-24 14:39:23 +0100

    Created the test for Luhn validaton when fails

--- js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
+++ js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
@@ -1,9 +1,9 @@
 import { expect, jest } from '@jest/globals'
 
 
-//import { SwedishSocialSecurityNumber } from "../src/correct/SwedishSocialSecurityNumber"
+import { SwedishSocialSecurityNumber } from "../src/correct/SwedishSocialSecurityNumber"
 //import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoLenCheck'
-import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoTrim'
+//import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoTrim'
 //import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberNoLuhn'
 //import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberWrongYear'
 
@@ -11,7 +11,6 @@ describe("SwedishSocialSecurityNumber Tests", () => {
 
   const correctFormat = "890201-3286"
   const correctFormatWithSpaces = " 890201-3286"
-  const ssnWithInvalidLuhn = "890201-3285"
 
   let mockHelper
   
@@ -30,8 +29,6 @@ describe("SwedishSocialSecurityNumber Tests", () => {
   test('should throw if length is incorrect', () => {
     mockHelper.isCorrectLength.mockReturnValue(false)
 
-    mockHelper.isCorrectFormat.mockReturnValue(true)
-
     expect(() => {
       new SwedishSocialSecurityNumber(correctFormat, mockHelper)
     }).toThrow("To short, must be 11 characters")
@@ -42,5 +39,14 @@ describe("SwedishSocialSecurityNumber Tests", () => {
 
     expect(SSSN.getYear()).toBe('89')
   })
+
+  test('should not throw error when Luhn validation fails', () => {
+    mockHelper.luhnisCorrect.mockReturnValue(false)
+
+    expect(() => {
+        new SwedishSocialSecurityNumber(correctFormat, mockHelper)
+    }).toThrow("Invalid SSN according to Luhn's algorithm")
+  })
+
 
 })
